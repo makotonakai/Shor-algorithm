7dc87804f2353c9b4691ad835f27ae2841bcad3b
MakotoNakai <45162150+MakotoNakai@users.noreply.github.com>
2019-01-05 22:27:59 +0900

    Update period.py

--- period.py
+++ period.py
@@ -1,5 +1,11 @@
+# N is the number you want to factor
+N = 14
+# a is the number which has no common divisor with N, and you want to power
+a = 5
+
+# I just put 21 because I thought I could get the answer from somewhere between 1 and 20
 for i in range(1,21):
-	if((5 ** i)%14 == 1):
+	if((a ** i)%N == 1):
 		print(i)
 		break
 		
